f9129dd4c53b173d48f6a8c0aa0c51e5eb465f7f
Rubylenshy <reztomoloju@gmail.com>
2022-10-11 22:31:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Frontend Mentor - Expenses Chart Component
+
+![Design preview for the Expenses Chart Component coding challenge](assets/design/desktop-design.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Flexbox
+- ❤️ Reuben
+
+### I learnt
+
+### Links
+
+- Live Site URL: [Expenses Chart Component](https://rubylenshy.github.io/expenses-chart-component/)
+
+## Author
+
+- Frontend Mentor - [@Rubylenshy](https://www.frontendmentor.io/profile/Rubylenshy)
+- Twitter - [@tomoloj_](https://www.twitter.com/tomoloj_)
---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+const chart = document.querySelector('.chart')
+const dollarValue = chart.querySelectorAll('.value')
+const bar = chart.querySelectorAll('.bar')
+const day = chart.querySelectorAll('p')
+
+//Method 1
+// fetch('../data.json')
+// .then((reponse)=>reponse.json())
+// .then((json)=>console.log(json));
+
+
+//Method 2
+// let file = '../data.json';
+extract('../data.json');
+
+async function extract(file){
+    let Obj1 = await fetch(file);
+    let myJson = await Obj1.text();
+    // console.log(myJson);
+
+    const jsonExtracted = JSON.parse(myJson);
+    console.log(jsonExtracted);
+
+    for (let i = 0; i < dollarValue.length; i++) {
+        dollarValue[i].textContent = '$' + jsonExtracted[i].amount;
+        day[i].textContent = jsonExtracted[i].day;
+        bar[i].style.height = jsonExtracted[i].amount*3 + 'px';
+
+        const nowDay = new Date().getUTCDay();
+        bar[nowDay].style.backgroundColor = 'hsl(186, 34%, 60%)';
+    }
+
+}
+
+//JSON parse(); stringify();
+var obj = {name: 'Fred', age: 20, study: 'Civic Education'};
+var s = JSON.stringify(obj);
+var q = JSON.parse(s);
+for (const key in q) {
+    if (Object.hasOwnProperty.call(q, key)) {
+        // console.log(`${key}: ${q[key]}`);
+    }
+}
